ac7abbe135eef2f347756e9e6fcbad8fccca22c7
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-07-28 20:17:31 +0700

    Update Orb.cs

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -21,10 +21,6 @@ public class Orb : Projectile
         {
             targetPos = target.transform.position;
             direction = targetPos - transform.position;
-            /*
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
-            */
         }
         else
         {
@@ -39,6 +35,7 @@ public class Orb : Projectile
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+
         if (Time.time >= nextEffectTime)
         {
             nextEffectTime = Time.time + effectTime;
