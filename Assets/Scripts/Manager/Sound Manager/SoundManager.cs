f698a6c9871d081a2c6b764d9ea1bf17e06e10b0
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-01 21:50:03 +0700

    fix cursor

--- Assets/Scripts/Manager/Sound Manager/SoundManager.cs
+++ Assets/Scripts/Manager/Sound Manager/SoundManager.cs
@@ -15,7 +15,7 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
