2e7b9dabb0be54bd6ec92b458b582f273bf1400d
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-01 15:46:54 +0700

    Add audio level save prefs

---
+++ Assets/Scripts/PreferencesManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreferencesManager : MonoBehaviour
+{
+    string audioKey = "Audio Level";
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPrefs();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SavePrefs();
+    }
+
+    public void SavePrefs()
+    {
+        PlayerPrefs.SetFloat(audioKey, SoundManager.Instance.volumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadPrefs()
+    {
+        float audioValue = PlayerPrefs.GetFloat(audioKey, 0);
+        SoundManager.Instance.volumeSlider.value = audioValue;
+    }
+}
