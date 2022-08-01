bd62b585fec49ee8a458d2835cf26f5e2acc8c67
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-01 16:18:10 +0700

    Change to Axie's cursor

---
+++ Assets/Scripts/Manager/MouseManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    public Texture2D defaultCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
