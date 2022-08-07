d6d786826a1c8418cdeb22b2a4c63b1c788c22f0
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-07 17:27:04 +0700

    Add buba

---
+++ Assets/Scripts/Projectile/SmashGround.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashGround : Projectile
+{
+    public float effectRadius;
+    public float bonusDamagePerEnemy;
+    int enemyinRange = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target != null)
+        {
+            transform.position = target.transform.position;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        bonusDamagePerEnemy *= holder.GetComponent<Champion>().currentLevel;
+        EffectEnemy();
+    }
+    
+    void EffectEnemy()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject t in targets)
+        {
+            if (Vector2.Distance(t.transform.position, transform.position) <= effectRadius)
+            {
+                enemyinRange++;
+            }
+        }
+
+        foreach (GameObject t in targets)
+        {
+            if (Vector2.Distance(t.transform.position, transform.position) <= effectRadius)
+            {
+                t.GetComponent<Enemy>().TakeDamage(damage + bonusDamagePerEnemy * enemyinRange, holder.GetComponent<LivingEntity>());
+            }
+        }
+    }
+}
