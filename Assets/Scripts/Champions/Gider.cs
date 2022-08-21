6c7d6c971d67179c3b7e636c81e3bc170b9b8781
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-21 11:20:24 +0700

    a

--- Assets/Scripts/Champions/Gider.cs
+++ Assets/Scripts/Champions/Gider.cs
@@ -16,11 +16,11 @@ public class Gider : Champion
             closestDistance = Vector2.Distance(transform.position, closestTarget.transform.position);
         }
         if (Time.time >= nextAttackTime)
-        {
-            nextAttackTime = Time.time + cooldownTime;
+        { 
             if (closestTarget != null && closestDistance <= championData.range && closestTarget)
             {
                 StartCoroutine(ShootIE(0));
+                nextAttackTime = Time.time + cooldownTime;
             }
         }
     }
---
+++ Assets/Scripts/Projectile/GiderProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GiderProjectile : Projectile
+{
+    public float lifeTime;
+    public GameObject giderFrame;
+    public GameObject giderGold;
+
+    void Start()
+    {
+        transform.position = holder.transform.position;
+        Vector3 direction = target.transform.position - holder.transform.position;
+        RotateToDirection(direction);
+        giderFrame.transform.DOScaleY(0.15f, 0.2f);
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        giderFrame.transform.DOKill();
+    }
+}
