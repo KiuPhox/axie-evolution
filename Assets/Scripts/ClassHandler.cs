1b0f311ddf94fb138c1672d0113abc7ff367d0bc
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-08-23 11:12:15 +0700

    Update ClassHandler.cs

--- Assets/Scripts/ClassHandler.cs
+++ Assets/Scripts/ClassHandler.cs
@@ -5,11 +5,11 @@ using TMPro;
 public class ClassHandler : MonoBehaviour
 {
     string beast = "<color=#FAE75A>Beast Effect:</color> Increase 30% defense for all axies";
-    string aquatic = "<color=#FAE75A>Aquatic Effect:</color> Slow enemies for 30%";
-    string bug = "<color=#FAE75A>Bug Effect:</color> Increase 30% damage for Bug class axie";
-    string bird = "<color=#FAE75A>Bird Effect:</color> Descrease 30% attack speed for Bird class axie";
-    string plant = "<color=#FAE75A>Plant Effect:</color> Increase 25% max health for all axies";
-    string reptile = "<color=#FAE75A>Reptile Effect:</color> Ignore enemies sheild";
+    string aquatic = "<color=#2FD4FD>Aquatic Effect:</color> Slow enemies for 30%";
+    string bug = "<color=#FD2423>Bug Effect:</color> Increase 30% damage for Bug class axie";
+    string bird = "<color=#F64FA9>Bird Effect:</color> Descrease 30% attack speed for Bird class axie";
+    string plant = "<color=#5FE722>Plant Effect:</color> Increase 25% max health for all axies";
+    string reptile = "<color=#9B5CCC>Reptile Effect:</color> Ignore enemies sheild";
 
     public GameObject classDescriptionUI;
     public TMP_Text descriptionText;
